2fc76b304e96edba690dfd4afb133612671b259b
Elad Fisher <tkgs444@gmail.com>
2025-11-29 19:33:56 +0200

    Creating Prefabs for Health-pickup and Full Health Pick-up with animation, and script to add health to player

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -4,6 +4,10 @@ public class HealthPickup : MonoBehaviour
 {
     public int m_healthAmount = 1; // Amount of health to restore
 
+    public GameObject PickupEffect; // Effect to play when the pickup is collected
+
+    public bool isFullHealth = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player")) // Check if the colliding object has the "Player" tag
@@ -15,8 +19,13 @@ public class HealthPickup : MonoBehaviour
                 return; // If the player's health is already at maximum, do nothing
             }
             // Increase the player's health and destroy the pickup
+            if(isFullHealth)
+            {
+                m_healthAmount = PlayerHealthManager.Instnace.GetMaxHealth(); // Set health amount to max health if isFullHealth is true
+            }
             PlayerHealthManager.Instnace.IncreaseHealth(m_healthAmount); // Increase the player's health by the specified amount
             Destroy(gameObject); // Destroy the health pickup object after it has been collected
+            Instantiate(PickupEffect, transform.position, transform.rotation); // Instantiate the pickup effect at the pickup's position
         }
     }
 }
